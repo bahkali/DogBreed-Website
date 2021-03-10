accc91d52d37b2fcf86b6b7c29f73e13fa0e6d63
kaly <kalimamadou@gmail.com>
2021-03-09 20:27:00 -0800

    Initialize the flutter app and added some assets

---
+++ dog_breed_app/android/app/src/main/kotlin/com/kmadstudio/dogbreedapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kmadstudio.dogbreedapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dog_breed_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:dogbreedapp/MySplashScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dog Breed Recognizer',
+
+      home: MySplashScreen(),
+    );
+  }
+}
